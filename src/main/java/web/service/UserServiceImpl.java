05001f32a048aa17c45cd3f0286a8aea466a854e
say-ilyas <102316165+say-ilyas@users.noreply.github.com>
2022-08-26 16:29:18 +0300

    Initial commit

---
+++ src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDAO;
+import web.model.User;
+import javax.validation.Valid;
+import java.util.List;
+
+@Service
+
+public class UserServiceImpl implements UserService {
+
+    private final UserDAO userDAO;
+
+    @Autowired
+    public UserServiceImpl(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    @Transactional
+    public List<User> getAllUsers() {
+        return userDAO.getAllUsers();
+    }
+
+    @Override
+    @Transactional
+    public Object getUserById(long id) {
+        return userDAO.getUserById(id);
+    }
+
+    @Override
+    @Transactional
+    public void addUser(User user) {
+        userDAO.addUser(user);
+    }
+
+    @Override
+    @Transactional
+    public void removeUser(long id) {
+        userDAO.removeUser(id);
+    }
+
+    @Override
+    @Transactional
+    public void updateUser(@Valid User user) {
+        userDAO.updateUser(user);
+    }
+}
